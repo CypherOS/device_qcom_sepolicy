a9382942229856e841de655dce24b38a787d8768
Chris <chris@cypheros.co>
2018-06-25 12:41:42 -0400

    Remove external aoscp sepolicy include
    This is moved to vendor and included by default in build
    
    Change-Id: I20ca28cd5e29651caa9162dda4abe3fdc25c7df3

--- sepolicy.mk
+++ sepolicy.mk
@@ -17,5 +17,3 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
-
--include vendor/aoscp-sepolicy/qcom/sepolicy.mk
